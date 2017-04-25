fa5f4163826f56ebfd3182d1b6bc679950a7fbaf
Linfeng Wang <wanggoudaner@gmail.com>
2017-04-24 20:41:27 -0400

    register & login

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+require 'authenticationFNS.php';
+require 'dbFNS.php';
+
+if (!$connection = @ mysqli_connect($hostName, $userName, $password))
+    die("Cannot connect");
+
+// Clean the data collected in the <form>
+$loginUsername = mysqlclean($_POST, "loginUsername", 40, $connection);
+$loginPassword = mysqlclean($_POST, "loginPassword", 40, $connection);
+
+if (!mysqli_select_db($connection, $databaseName))
+    showerror();
+
+session_start();
+
+// Authenticate the user
+if (authenticateUser($connection, $loginUsername, $loginPassword)) {
+    // Register the loginUsername
+    $_SESSION["loginUsername"] = $loginUsername;
+
+    // // Register the IP address that started this session
+    // $_SESSION["loginIP"] = $_SERVER["REMOTE_ADDR"];
+
+    $_SESSION["loginPassword"] = $loginPassword;
+    // Relocate back to the first page of the application
+    header("Location: home.html");
+    exit;
+
+} else {
+    // The authentication failed: setup a logout message
+    $_SESSION["message"] =
+        "Could not connect to the application as '{$loginUsername}'";
+
+    // Relocate to the logout page
+    header("Location: 404.html");
+    exit;
+}
+?>
+
---
+++ register.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @param $connection
+ * @param $username
+ * @param $password
+ * @return bool
+ */
+function registerUser($connection, $username, $email, $password, $hometown, 
+        $truename, $gender)
+{
+    // Test the username and password parameters
+    if (!isset($username) || !isset($gender))
+        return false;
+
+    //date_default_timezone_set(‘America/New_York’);
+    //string date(string $format [, int $timestamp ]);
+    //$time = date('Y-m-d H:i:s');
+
+    // if (!isset($hometown) && !isset(truename))
+    //     $query = "INSERT INTO `user` VALUES ('{$username}', null, '{$password}', 
+    //     '{$email}', '{$gender}', null, '2014-01-03 12:04:23')";
+    // else if (!isset($hometown) && isset(truename))
+    //     $query = "INSERT INTO `user` VALUES ('{$username}', '{$truename}', '{$password}', 
+    //     '{$email}', '{$gender}', null, '2014-01-03 12:04:23')";
+    // else if (!isset($hometown) && !isset(truename))
+    //     $query = "INSERT INTO `user` VALUES ('{$username}', null, '{$password}', 
+    //     '{$email}', '{$gender}', '{$hometown}', '2014-01-03 12:04:23')";
+
+    // Formulate the SQL find the user
+    else $query = "INSERT INTO `user` VALUES ('{$username}', '{$truename}', '{$password}', 
+        '{$email}', '{$gender}', '{$hometown}', '2014-01-03 12:04:23')";
+
+    // Execute the query
+    if (!$result = @ mysqli_query($connection, $query))
+        //-------------show a window of it!!-------------
+        showerror();
+    else
+        return true;
+}
+
+
+?>
