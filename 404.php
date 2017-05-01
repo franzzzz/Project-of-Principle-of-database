959750afcecc2f4046ae510127017f0d091980ee
Linfeng Wang <wanggoudaner@gmail.com>
2017-05-01 13:36:21 -0400

    Change 404
    404 html -> php

--- 404.php
+++ 404.php
@@ -1,10 +1,33 @@
-<!DOCTYPE html>
+<?php
+  session_start();
+  require 'dbFNS.php';
+  if (!$connection = mysqli_connect($hostName, $userName, $password))
+    die("Cannot connect");
+  mysqli_select_db($connection, $databaseName);
+  $loginUsername = $_SESSION["loginUsername"];
+  
+  //user's profile
+  $query_my_profile = "SELECT createtime FROM user WHERE username = '{$loginUsername}'";
+  $result_my_profile = @ mysqli_query($connection, $query_my_profile);
+  $line_my_profile = mysqli_fetch_array($result_my_profile);
+  $_SESSION["loginTime"] = $line_my_profile[0];
+  $php_createtime_timestamp = strtotime($_SESSION["loginTime"]);
+  date_default_timezone_set('America/New_York');
+  $restday = floor((time()-$php_createtime_timestamp)/3600);
+  $createtime = date('F d, Y', $php_createtime_timestamp);
+
+  //show all project here
+  $query_show_all_pro = "SELECT pname, username, maxfund, endtime, moneysum FROM project WHERE status = 'funding'";
+  $result_show_all_pro = @ mysqli_query($connection, $query_show_all_pro);
+
+?>
+
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Campaigs</title>
+<title>CrowdFunding</title>
 
 <!-- Css (necessary Css) -->
 <link href="assets/css/bootstrap.min.css" rel="stylesheet">
@@ -22,10 +45,6 @@
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <div class="wrapper"> 
@@ -39,41 +58,27 @@
         <nav class="navigation">
           <ul>
             <li><a href="home.php">Home</a></li>
-            <li><a href="#">Discover</a>
-              <ul class="sub-dropdown">
-                <li><a href="listing-grid.html">Grid View</a></li>
-                <li><a href="listing.php">List view</a></li>
-                <li><a href="detail.html">Detail Page</a></li>
-              </ul>
-            </li>
+            <li><a href="listing.php">Discover</a></li>
             <li><a href="creators.html">Creators</a></li>
             <li><a href="supporters.html">Supporters</a></li>
             <li><a href="#">Pages</a>
               <ul class="sub-dropdown">
-                <li><a href="about.html">About us</a></li>
-                <li><a href="faq.html">FAQ’s</a></li>
-                <li><a href="contect.html">Contect</a></li>
+                <!--<li><a href="about.html">About us</a></li>-->
+                <!--<li><a href="faq.html">FAQ’s</a></li>-->
+                <!--<li><a href="contect.html">Contect</a></li>-->
                 <li><a href="sign.php">Create an Account</a></li>
                 <li><a href="sign.php">Sign In</a></li>
                 <li><a href="404.php">404 Page</a></li>
-                <li><a href="under.html">under-construction</a></li>
-                <li><a href="terms.html">Terms &amp; Conditions</a></li>
-                <li><a href="pricing.html">Price &amp; Packges</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="site-map.html">Site Map</a></li>
+                <!--<li><a href="under.html">under-construction</a></li>-->
+                <!--<li><a href="terms.html">Terms &amp; Conditions</a></li>-->
+                <!--<li><a href="pricing.html">Price &amp; Packges</a></li>-->
+                <!--<li><a href="services.html">Services</a></li>-->
+                <!--<li><a href="site-map.html">Site Map</a></li>-->
                 <li><a href="result.html">Result</a></li>
                 <li><a href="donate.html">Donate</a></li>
                 <li><a href="user-detail2.html">user detail2</a></li>
               </ul>
             </li>
-            <li>
-				<a href="#">News</a>
-				<ul class="sub-dropdown">
-					<li><a href="bloglrag.html">News Listing</a></li>
-					<li><a href="blogmedium.html">News Medium</a></li>
-					<li><a href="blogdetail.html">News Detail</a></li>
-				</ul>
-			</li>
           </ul>
         </nav>
       </div>
@@ -92,12 +97,13 @@
               <img alt="#" src="assets/extra-images/user-img.jpg">
               <i class="icon-arrow-down8"></i>
               <div class="dropdown-area">
-                <h5>Mark Benson</h5>
-                <span>Member Since May 20, 2014</span>
+                <h5> <?php echo $_SESSION["loginUsername"]; ?> </h5>
+                <span> <?php echo 'Member Since '.$createtime; ?> </span>
+                <span> <?php echo $restday.'days ago.  Great!'; ?> </span>
                 <ul class="dropdown">
                   <li><a href="causes.html"><i class="icon-flag5"></i>My Causes</a></li>
                   <li><a href="saved.html"><i class="icon-file-text-o"></i>Saved Causes</a></li>
-                  <li><a href="my-donation.php"><i class="icon-file-text-o"></i>My Donations</a></li>
+                  <li><a href="my-donation.html"><i class="icon-file-text-o"></i>My Donations</a></li>
                   <li><a href="donation.html"><i class="icon-ticket6"></i>Donations</a></li>
                   <li><a href="profilesetting.html"><i class="icon-pie2"></i>Profile Settings</a></li>
                   <li><a href="create-new-cause.html"><i class="icon-plus6"></i>Create New</a></li>
@@ -107,19 +113,18 @@
             </li>
           </ul>
         </div> 
-        <a href="sign.php" class="free-btn">Start for Free</a> </div>
     </div>
     <div class="mob-nav"></div>
     </div>
   </header>
-  <!-- Header -->
+  <!-- Header --> 
 	<div class="breadcrumb-sec" style="background:url(assets/extra-images/banner.jpg) no-repeat; background-size:100% auto; min-height:157px!important;">
 		<div class="absolute-sec">
 			<div class="container">
 				<div class="cs-table">
 					<div class="cs-tablerow">
 						<div class="pageinfo page-title-align-left">
-							<h1 style="color:#fff !important; text-transform:none;">Search Result</h1>
+							<h1 style="color:#fff !important; text-transform:none;">to be written</h1>
 						</div>
 					</div>
 				</div>
@@ -133,99 +138,32 @@
 			<div class="page-section">
 				<div class="container">
 					<div class="row">
-						<div class="section-fullwidth">
-							 <div class="cs-content-holder">
-								 <div class="row">
-								 <section class="cs-result  site-map">
+						 <div class="section-fullwidth col-lg-12">
+						 	<div class="cs-content-holder">
+								<div class="row">
 									<div class="col-lg-12">
-										<div class="content">
-											<h2>Sitemap</h2>
-											<div class="row">
-												<div class="col-lg-3 col-md-3 col-sm-6">
-													<div class="site-link">
-														<h3>Desk References</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-														<h3>List Styles</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-													</div>
-												</div>
-												<div class="col-lg-3 col-md-3 col-sm-6">
-													<div class="site-link">
-														<h3>List Styles</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-														<h3>Faculty & Staff</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-													</div>
-												</div>
-												<div class="col-lg-3 col-md-3 col-sm-6">
-													<div class="site-link">
-														<h3>Academics</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-														<h3>Administration & Services</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
-													</div>
-												</div>
-												<div class="col-lg-3 col-md-3 col-sm-6">
-													<div class="site-link">
-														<h3>Academics</h3>
-														<ul>
-															<li><a href="#">Overview</a></li>
-															<li><a href="#">Facts & Figures</a></li>
-															<li><a href="#">Princeton's History</a></li>
-															<li><a href="#">University Governance</a></li>
-															<li><a href="#">Our Neighborhood</a></li>
-															<li><a href="#">About This Site</a></li>
-														</ul>
+										<div class="page-not-found">
+										  <div class="cs-icon">
+											<img src="assets/extra-images/error-img.png" alt="#">
+										  </div>
+										  <div class="cs-content404">
+											<h2>Oops, the page you are looking for might have<br>been removed</h2>
+										  <div class="desc">
+											  <div class="site-maps-links">
+													<h6>Visit some of our working pages</h6>
+													  <ul>
+														  <li><a href="#">Home Page</a></li>
+														  <!--<li><a href="#">About us</a></li>-->
+														  <!--<li><a href="#">Services</a></li>-->
+														  <!--<li><a href="#">Portfolio</a></li>-->
+													  </ul>
 													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-								 </section>
-								 </div>
-							 </div>
+								</div>
+							</div>
 						 </div> 
 					</div>
 				</div>
@@ -238,42 +176,42 @@
 	<footer id="footer-sec">
 		<div class="container">
 			<div class="row">
-				<div class="ads-sec col-lg-12"> <img src="assets/images/ads.jpg" alt="" > </div>
+				<div class="ads-sec col-lg-12"><a href="http://www.nike.com/us/en_us/c/jordan"><img src="assets/images/ads.jpg" alt="" ></a> </div>
 				<div class="col-lg-12">
 					<div class="tweet-sec">
-						<p><i class="twitter-icon icon-twitter2"></i> Thing anyone did today was get ready for #Scandal <a href="#">http://t.co/UfiNiBLWlZ http://t.co/Fvggnf320f</a> <span class="by">Boxtheme</span> <span class="time">10 hours ago</span></p>
+						<p><i class="twitter-icon icon-twitter2"></i> Share my passion and meet with others that love #Jordans <a href="https://twitter.com/ajordanrelease">  For More ... </a></p>
 					</div>
 				</div>
 				<aside class="col-lg-4 col-md-4 col-sm-6 widget widget_categories">
 					<div class="widget-section-title"> <strong class="title">Campaigning</strong> </div>
 					<ul>
-						<li><a href="#">Contributing</a></li>
+						<li><a href="#">something to write?</a></li>
 						<li><a href="#">Publishing</a></li>
-						<li><a href="#">Explore Partner Pages</a></li>
-						<li><a href="#">Music</a></li>
-						<li><a href="#">Daily Inspiration</a></li>
-						<li><a href="#">Film and Theatre</a></li>
-						<li><a href="#">Sign Up Now</a></li>
-						<li><a href="#">Food and Drink</a></li>
-						<li><a href="#">Private, secure, spam-free</a></li>
-						<li><a href="#">Sports</a></li>
-						<li><a href="#">School</a></li>
+						<!--<li><a href="#">Explore Partner Pages</a></li>-->
+						<!--<li><a href="#">Music</a></li>-->
+						<!--<li><a href="#">Daily Inspiration</a></li>-->
+						<!--<li><a href="#">Film and Theatre</a></li>-->
+						<!--<li><a href="#">Sign Up Now</a></li>-->
+						<!--<li><a href="#">Food and Drink</a></li>-->
+						<!--<li><a href="#">Private, secure, spam-free</a></li>-->
+						<!--<li><a href="#">Sports</a></li>-->
+						<!--<li><a href="#">School</a></li>-->
 					</ul>
 				</aside>
 				<aside class="widget col-lg-4 col-md-4 col-sm-6 widget_categories">
 					<div class="widget-section-title"> <strong class="title">Contributing</strong> </div>
 					<ul>
-						<li><a href="#">Crowdfunder API - Beta</a>
-						<li><a href="#">How crowdfunding works</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Creating a project</a></li>
-						<li><a href="#">Guides</a></li>
-						<li><a href="#">Supporting a project</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">Guidelines</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Jobs</a></li>
-						<li><a href="#">Partners</a></li>
+						<li><a href="#">something to write?</a>
+						<!--<li><a href="#">How crowdfunding works</a></li>-->
+						<!--<li><a href="#">About</a></li>-->
+						<!--<li><a href="#">Creating a project</a></li>-->
+						<!--<li><a href="#">Guides</a></li>-->
+						<!--<li><a href="#">Supporting a project</a></li>-->
+						<!--<li><a href="#">Blog</a></li>-->
+						<!--<li><a href="#">Guidelines</a></li>-->
+						<!--<li><a href="#">Contact</a></li>-->
+						<!--<li><a href="#">Jobs</a></li>-->
+						<!--<li><a href="#">Partners</a></li>-->
 					</ul>
 				</aside>
 				<aside class="widget col-lg-4 col-md-4 col-sm-6 widget_newsletter">
@@ -301,14 +239,14 @@
 						<nav class="footer-nav">
 							<ul>
 								<li><a href="#">Home</a></li>
-								<li><a href="#">Categories</a></li>
-								<li><a href="#">Showrooms</a></li>
-								<li><a href="#">Agents</a></li>
-								<li><a href="#">Blog</a></li>
-								<li><a href="#">Contact us</a></li>
+								<li><a href="#">something to write</a></li>
+								<!--<li><a href="#">Showrooms</a></li>-->
+								<!--<li><a href="#">Agents</a></li>-->
+								<!--<li><a href="#">Blog</a></li>-->
+								<!--<li><a href="#">Contact us</a></li>-->
 							</ul>
 						</nav>
-						<p>©2014 Box Theme All rights reserved. Design by <a href="http://www.jqueryfuns.com">Chimp Studio</a></p>
+						<p>©2017  All rights reserved by MXB</p>
 					</div>
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-6">
